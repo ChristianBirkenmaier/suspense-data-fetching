ec9d261bcb876faa8d75bad9a1c017ec608e3b93
Christian Birkenmaier <christian.birkenmaier@vonaffenfels.de>
2020-06-03 13:18:55 +0200

    first commit

---
+++ lib/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from "react";
+// import { PendingTodos, CompletedTodos } from "./";
+import PendingTodos from "./PendingTodos";
+import CompletedTodos from "./CompletedTodos";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Here are your Todos for today</h1>
+      <p>Click on any todo to view more details about it</p>
+
+      <h3>Pending Todos</h3>
+      <Suspense fallback={<h1>Loading Pending Todos...</h1>}>
+        <PendingTodos />
+      </Suspense>
+
+      <h3>Completed Todos</h3>
+      <Suspense fallback={<h1>Loading Completed Todos...</h1>}>
+        <CompletedTodos />
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
